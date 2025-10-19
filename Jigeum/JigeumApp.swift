2645c2190f57c95cbc28ac87c117d9788d829208
seeeeeeong <lsinsung@gmail.com>
2025-10-19 19:29:16 +0900

    Initial Commit

---
+++ Jigeum/JigeumApp.swift
@@ -0,0 +1,17 @@
+//
+//  JigeumApp.swift
+//  Jigeum
+//
+//  Created by leesinseong on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct JigeumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
